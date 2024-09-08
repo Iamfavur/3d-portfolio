b1dd19a4f31f5a2952cce930149cfc2935829c59
mac <mac@omnihoeliests-macbook.local>
2024-09-08 07:10:33 +0100

    portfolioo

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/3d-portfolio/", // YOUR REPO NAME HERE
+  base: "/3d-portfolioo/", // YOUR REPO NAME HERE
   plugins: [react()],
 });
