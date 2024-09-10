1aecec5e6f4fd71c3bcf7c139733e3c82fddfc94
mac <mac@omnihoeliests-macbook.local>
2024-09-10 10:55:01 +0100

    loader removed for earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,7 +1,7 @@
 import React,{Suspense} from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls,Preload, useGLTF } from '@react-three/drei';
-import Loader from '../loader';
+// import Loader from '../Loader';
 
 const Earth = () => {
   const earth = useGLTF('./planet/scene.gltf')
@@ -28,7 +28,7 @@ const EarthCanvas =()=>{
         position:[-4,3,6]
       }}
     >
-      <Suspense fallback={<Loader/>}>
+      <Suspense >
         <OrbitControls
           autoRotate
           enableZoom={false}
